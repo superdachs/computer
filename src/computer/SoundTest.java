31aaa04b89eafcd7dc7ba081fbe45a5239f2d60b
stk <stk@prometheus>
2013-08-06 15:41:50 +0200

    kleine änderungen

--- src/computer/SoundTest.java
+++ src/computer/SoundTest.java
@@ -5,7 +5,6 @@
 package computer;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -15,6 +14,8 @@ import java.util.logging.Logger;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineListener;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.TargetDataLine;
 
@@ -29,42 +30,25 @@ public class SoundTest {
     int channels = 1;
     boolean signed = true;
     boolean bigEndian = true;
-    AudioFormat format = new AudioFormat(sampleRate,
-            sampleSizeInBits, channels, signed, bigEndian);
+    AudioFormat format = new AudioFormat(AudioFormat.Encoding.ALAW, sampleRate,
+            sampleSizeInBits, channels, channels, sampleRate, bigEndian);
     DataLine.Info info = new DataLine.Info(
             TargetDataLine.class, format);
     TargetDataLine line;
 
     public SoundTest() {
-        System.out.println("Soundtest!");
         try {
+            System.out.println("Soundtest!");
+
             line = (TargetDataLine) AudioSystem.getLine(info);
             System.out.println("Line found: " + line.getLineInfo().toString());
             line.open(format);
             line.start();
 
-            int bufferSize = (int) format.getSampleRate()
-                    * format.getFrameSize();
-            byte buffer[] = new byte[bufferSize];
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            Date start = new Date();
-            while (new Date().getTime() < (start.getTime() + 10000)) {
-                int count = line.read(buffer, 0, buffer.length);
-                if (count > 0) {
-                    out.write(buffer, 0, count);
-                }
+            while (true) {
             }
 
-            OutputStream outputStream = new FileOutputStream("audiofile.wav");
-            out.writeTo(outputStream);
-
-
-            out.close();
-
-
         } catch (LineUnavailableException ex) {
-            Logger.getLogger(SoundTest.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
             Logger.getLogger(SoundTest.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
