561c65a8e3afde3b01ec80d5b22b44278bb64c4c
Stefan Kauerauf <stefan.kauerauf@shd-online.de>
2015-10-02 16:05:52 +0200

    Fake Constants implementation for Wolfram Alpha.

---
+++ src/computer/ConstantsImpl.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package computer;
+
+/**
+ *
+ * @author kauerauf
+ */
+public class ConstantsImpl {
+    
+     public String getWolframAlphaAppID() {
+         return "abcdefg1234567";
+     }
+    
+}
